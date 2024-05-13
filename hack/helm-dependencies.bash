6a199dbe60dffcfd0adf5d86dd27c760adaf652d
Gábor Almádi <gabor.almadi@t-systems.com>
2024-05-13 11:26:30 +0200

    fix: add runix helm repo to the dependency hack script

--- hack/helm-dependencies.bash
+++ hack/helm-dependencies.bash
@@ -5,6 +5,7 @@ if ! helm repo list ; then
   echo "Need to add repos"
   helm repo add tractusx https://eclipse-tractusx.github.io/charts/dev
   helm repo add hashicorp https://helm.releases.hashicorp.com
+  helm repo add runix https://helm.runix.net
 fi
 
 # This hack script will download all chart/umbrella dependency charts.
